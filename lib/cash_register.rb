f756644104c93dbcfaf2a66e0617068144367b5b
haleighdalke <haleighdalke@gmail.com>
2020-06-03 21:24:57 -0700

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,8 +4,8 @@ class CashRegister
     # define setters/getters Macro
     # define @@ total array of items?
     # define initiation method
-    attr_accessor :total
-    attr_reader :discount, :items
+    attr_accessor :items, :discount, :total, :last_transaction
+
 
     def initialize(discount=nil)
         @total = 0
@@ -17,10 +17,10 @@ class CashRegister
     # also accepts an optional quantity (FAILED - 5)
     # doesn't forget about the previous total (FAILED - 6)
     def add_item(title, price, quantity=1)
-        @prev_total = @total
+        self.last_transaction = self.total
 
-        quantity.times{@items << title}
-        @total += price*quantity
+        quantity.times{self.items << title}
+        self.total += price*quantity
     end
 
 #  the cash register was initialized with an employee discount
@@ -30,9 +30,9 @@ class CashRegister
 #   the cash register was not initialized with an employee discount
 #     returns a string error message that there is no discount to apply (FAILED - 10)
     def apply_discount
-        if discount != nil
-            @total = (@total - (@total * (@discount.to_f / 100))).to_i
-            return "After the discount, the total comes to $#{@total}."
+        if self.discount != nil
+            self.total = (self.total - (self.total * (self.discount.to_f / 100))).to_i
+            return "After the discount, the total comes to $#{self.total}."
         else
             return "There is no discount to apply."
         end
@@ -41,15 +41,10 @@ class CashRegister
     # subtracts the last item from the total (FAILED - 12)
     # returns the total to 0.0 if all items have been removed (FAILED - 13)
     def void_last_transaction
-        if @prev_total == 0
-            @total = 0.0
+        if self.last_transaction == 0
+            self.total = 0.0
         else
-            @total = @prev_total
+            self.total = self.last_transaction
         end
     end
 end
-
-
-# my_register = CashRegister.new
-
-# binding.pry
